160855bd2bd361615bacf298e48f137384f48a8e
Tony Han <h.bing612@gmail.com>
2013-10-29 11:07:06 +0800

    remove the Forwarable and handle array in #[] method

--- objectable_hash.rb
+++ objectable_hash.rb
@@ -1,7 +1,4 @@
-require 'forwardable'
 class ObjectableHash
-  extend Forwardable
-  def_delegators :@obj, :[]
 
   def initialize( obj )
     @obj = obj
@@ -27,5 +24,14 @@ class ObjectableHash
     return true if other.equal?(self)
     return false unless other.instance_of?(self.class)
     self.val == other.val
+  end
+
+  def [](key)
+    val = @obj[key]
+    if val.is_a?(Array) or val.is_a?(Hash)
+      ObjectableHash.new(val)
+    else
+      val
+    end
   end
 end
